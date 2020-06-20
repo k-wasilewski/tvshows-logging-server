57b440bc281bb2ec65014c87423b2a0430c11054
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-20 19:15:14 +0200

    initial commit

---
+++ src/main/java/com/example/tvshowsloggingserver/LoggingController.java
@@ -0,0 +1,33 @@
+package com.example.tvshowsloggingserver;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+@RestController
+public class LoggingController {
+    @PostMapping("/log")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public void log(@RequestBody String log) {
+        String systemuser = System.getProperty("user.name");
+
+        String LOGS_PATH = "/home/"+systemuser+"/tvshows-logs";
+
+        File uploadFolder = new File(LOGS_PATH);
+        if (!uploadFolder.exists()) {
+            uploadFolder.mkdirs();
+        }
+
+        try {
+            PrintWriter out = new PrintWriter(LOGS_PATH+File.separator+
+                    "tvshows-log-"+new java.util.Date()+".txt");
+            out.println(log);
+            out.close();
+        } catch (FileNotFoundException e) {}
+    }
+}
---
+++ src/test/java/com/example/tvshowsloggingserver/TvshowsLoggingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tvshowsloggingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvshowsLoggingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
